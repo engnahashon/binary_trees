bc7e6846c5921c812eeef4649cb9878b377d9f26
engnahashon <nahashon.ndirangu@outlook.com>
2023-03-01 10:12:25 +0000

    binary right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,9 +8,6 @@
   */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
-
 	binary_tree_t *new_node;
 
 	new_node = binary_tree_node(parent, value);
