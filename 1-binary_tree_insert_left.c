a8fb456ad160abf0f9bc5f085f1938518a1d85e2
engnahashon <nahashon.ndirangu@outlook.com>
2023-03-01 09:59:19 +0000

    binart

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,25 +23,3 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = new_node;
 	return (new_node);
 }
-/**
-  * binary_tree_node - function that creates a binary tree node
-  * @parent: pointer to the parent node of the node to create
-  * @value: value to put in the new node
-  *
-  * Return: pointer to new node or NULL on failure
-  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
-}
