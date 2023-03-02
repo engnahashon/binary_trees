d6283772edeec7f2e3dd5fd8f03606a9dddbbb84
engnahashon <nahashon.ndirangu@outlook.com>
2023-03-02 09:33:47 +0000

    leaves count

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,17 +7,11 @@
   */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
 	if (tree == NULL)
 		return (0);
-	if (tree)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			count++;
-		count = binary_tree_leaves(tree->left) + 1;
-		count = binary_tree_leaves(tree->right) + 1;
 
-	}
-	return (count);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
